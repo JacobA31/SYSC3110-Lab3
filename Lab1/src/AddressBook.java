6cdaf4f76817be50be4081298b34378fb6255291
Ihsan Jacob Aydin <91477863+JacobA31@users.noreply.github.com>
2024-09-19 09:18:40 -0400

    Update AddressBook.java
    Added new buddy

--- Lab1/src/AddressBook.java
+++ Lab1/src/AddressBook.java
@@ -18,10 +18,13 @@ public class AddressBook {
 
     public static void main(String[] args)
     {
-        BuddyInfo buddyInfo1 = new BuddyInfo("Jacob","666 Riverflow", 555);
+        BuddyInfo buddyInfo1 = new BuddyInfo("Jacob", "666 Riverflow", 555);
+        BuddyInfo buddyInfo2 = new BuddyInfo("Aydin","556 Livery", 666);
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddyInfo1);
         addressBook.removeBuddy(buddyInfo1);
+        addressBook.addBuddy(buddyInfo2);
+        System.out.println(addressBook.buddyInfo.get(0));
     }
 
 }
